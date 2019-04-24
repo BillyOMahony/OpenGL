13b2302eb605f03551ccb6716965752d900f6436
Billy O'Mahony <omahonybilly@gmail.com>
2019-04-24 14:19:44 +0100

    Created a buffer for a triangle and it's vertices

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -8,8 +8,11 @@ int main(void)
 
 	/* Initialize the library */
 	if (!glfwInit())
+	{
+		std::cout << "Could not initialise OpenGL" << std::endl;
+		std::cin.get();
 		return -1;
-
+	}
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
@@ -31,20 +34,30 @@ int main(void)
 
 	std::cout << "OpenGL has found: " << glGetString(GL_VERSION) << std::endl;
 
+	// Triangle Vertex locations. 
+	float TriangleVertices[6] = 
+	{
+		-1.f, -1.f,
+		 0.f,  0.f, 
+		 1.f, -1.f
+	};
+
+	// Create buffer
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	// Select the buffer, as in we're about to work on this buffer
+	// Bind to what you want to draw on, think of layers in photoshop
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	// Initialize buffer and give it data
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), TriangleVertices, GL_STATIC_DRAW);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-
-		glVertex2f(-1.f, -1.f);
-		glVertex2f(0.f, 0.f);
-		glVertex2f(0.5f, -0.5f);
-
-		glEnd();
-
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
